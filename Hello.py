7e5fa2f6e14a9b0c5dd6d302db72303c5209e277
AdiVishnia <adi.vishnia@gmail.com>
2025-07-27 16:09:15 +0300

    new folder

--- Hello.py
+++ Hello.py
@@ -2,15 +2,15 @@
 #my_list=[10,20,30,40,50]
 #print(my_list[1::-1])
 #print(my_list[2:3]==my_list[2])
-#squares=[x*x for x in range(1,6) if x%2==0]
-#print(squares)
-numbers=[1,2,3,4,5]
-squared=list(map(5,numbers))
-print(squared)
+squares=[x+1 for x in range(1,6) if x%2==0]
+print(squares)
+#numbers=[1,2,3,4,5]
+#squared=list(map(5,numbers))
+#print(squared)
 #f={"name": "Bob","name":"Clice"}
 #print(f["name"])
 #s=[80,"55",42]
 #print(s+s)
-my_tuple=(1,'hello','world')
+#my_tuple=(1,'hello','world')
 ##my_tuple[2]="gier"
 #print("Tech")
